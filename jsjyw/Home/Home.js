e3f089fa28149b786c1a9523ee05bc26e74f708c
zori0 <776021342@qq.com>
2016-07-02 17:16:20 +0800

    优化

--- jsjyw/Home/Home.js
+++ jsjyw/Home/Home.js
@@ -6,12 +6,18 @@ import {
     Image,
     TouchableOpacity,
     ScrollView,
+    TabBarIOS
 } from 'react-native'
 
 'use strict';
 import Util from './Util'
 import Header from './Header'
-
+import Banner from './Banner'
+import TabList from './TabList'
+import Floor from './Floor'
+import Floor_JZhuang from './Floor_JZhuang'
+import Floor_JDian from './Floor_JDian'
+import Floor_JZheng from './Floor_JZheng'
 
 export default class extends Component {
 
@@ -24,7 +30,53 @@ export default class extends Component {
 
     render(){
         return (
-            <View></View>
+            <View style={[styles.flex_column]}>
+                <Header />
+                <TabBarIOS style={[styles.tabBar]}>
+                    <TabBarIOS.Item
+                        title="首页"
+                        selected={true}
+                        icon={require('./images/TabBar/tabbar_one.imageset/tabbar_one.png')}
+                        selectedIcon={require('./images/TabBar/tabbar_one_selected.imageset/tabbar_one_selected.png')}
+                        tintColor="red"
+                        renderAsOriginal
+                    >
+                        <ScrollView style={[styles.flex_column]}>
+                            <View style={[styles.flex_column]}>
+                                <Banner banners={this.props.datas.banner.groupAttrList}/>
+                                <TabList tabList={this.props.datas.tabList.groupAttrList}/>
+                            </View>
+                            <View style={[styles.flex_column]}>
+                                <Floor houseIntr={this.props.datas.houseIntr}/>
+                                <Floor houseIntr={this.props.datas.secondHouse}/>
+                                <Floor houseIntr={this.props.datas.CZHouse}/>
+                                <Floor_JZhuang houseMagic={this.props.datas.houseMagic}/>
+                                <Floor_JDian houseElectrical={this.props.datas.houseElectrical} />
+                                <Floor_JDian houseElectrical={this.props.datas.houseFurniture} />
+                                <Floor_JZheng houseHeeping={this.props.datas.houseHeeping}/>
+                                <View style={[styles.flex_column]}>
+                                    <TouchableOpacity>
+                                        <Image source={require('../../view/images/lms.jpg')} style={[styles.bottomImage]}/>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                        </ScrollView>
+                    </TabBarIOS.Item>
+                    <TabBarIOS.Item
+                        title="预约单"
+                        icon={require('./images/TabBar/tabbar_two.imageset/tabbar_two.png')}
+                    />
+                    <TabBarIOS.Item
+                        title="订单"
+                        icon={require('./images/TabBar/tabbar_three.imageset/tabbar_three.png')}
+                    />
+                    <TabBarIOS.Item
+                        title="我"
+                        icon={require('./images/TabBar/tabbar_four.imageset/tabbar_four.png')}
+                    />
+
+                </TabBarIOS>
+            </View>
         )
     }
 
@@ -39,5 +91,13 @@ const styles = StyleSheet.create({
     flex_column:{
         flex:1,
         flexDirection:'column'
+    },
+    tabBar:{
+        height:80
+    },
+    bottomImage:{
+        width:Util.size.width,
+        resizeMode:'cover',
+        height:60
     }
 });
